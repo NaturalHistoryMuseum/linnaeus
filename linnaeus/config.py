3e96d329e0144bb2e2e019bff25535e4ac8c6c7b
Alice Butcher <alycejenni@gmail.com>
2018-06-06 14:56:46 +0100

    added logger

--- linnaeus/config.py
+++ linnaeus/config.py
@@ -1,6 +1,7 @@
 import os
 
 import yaml
+import logging
 
 
 class Config(object):
@@ -16,6 +17,14 @@ class Config(object):
         self.pixel_size = (self.pixel_width, self.pixel_height)
         self.max_ref_side = config_dict.get('max_ref_side', 100)
         self.saturation_threshold = config_dict.get('saturation_threshold', 50)
+        self.log_level = logging.getLevelName(config_dict.get('log_level', 'DEBUG').upper())
 
 
 constants = Config()
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(constants.log_level)
