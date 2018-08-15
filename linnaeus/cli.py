33202de40ff9f7fc79f238100d8a3963e6c3b5a0
Alice Butcher <alycejenni@gmail.com>
2018-08-15 23:20:28 +0100

    added a 'pipe' option to silence all output except file names

--- linnaeus/cli.py
+++ linnaeus/cli.py
@@ -5,46 +5,60 @@ import numpy as np
 import os
 from PIL import Image
 
-from linnaeus import Builder, MapFactory, SolveError
-from linnaeus.config import constants
-from linnaeus.models import ReferenceMap
-from linnaeus.preprocessing import BackgroundRemover, colourspace
+from linnaeus.config import constants, silence
 
 click_context = {
     'help_option_names': ['-h', '--help']
     }
 
 
-def setup_path(path):
+def echo(ctx, msg, **kwargs):
+    pipe = ctx.obj['pipe']
+    is_err = kwargs.get('err', False)
+    if (not pipe) or is_err:
+        click.echo(msg, **kwargs)
+
+
+def confirm(ctx, msg, **kwargs):
+    if not ctx.obj['pipe']:
+        return click.confirm(msg, **kwargs)
+    else:
+        return True
+
+
+def setup_path(ctx, path):
     """
     Aborts if the path already exists and should not be overwritten; otherwise creates
     necessary folders if not already present.
+    :param ctx: context
     :param path: path to check/setup
     :return: None
     """
     if os.path.exists(path):
-        click.confirm(f'{path} already exists. Overwrite?', abort=True)
+        confirm(ctx, f'{path} already exists. Overwrite?', abort=True)
     else:
         folders = os.path.sep.join(path.split(os.path.sep)[:-1])
         if folders != '':
             os.makedirs(folders, exist_ok=True)
 
 
-def deserialise(path, factory, callback=None, saveas=None):
+def deserialise(ctx, path, factory, callback=None, saveas=None):
     """
     Attempts to deserialise the file at the given path. If it is not the correct type,
     runs a callback function if defined, and aborts if not.
+    :param ctx: context
     :param path: the path to the file to try to deserialise
     :param factory: the MapFactory to use to deserialise (can just be MapFactory)
     :param callback: a function returning a Map
     :return: Map
     """
+    from linnaeus import MapFactory
     try:
         read_map = factory.deserialise(MapFactory.load_text(path))
         return read_map
     except (json.JSONDecodeError, UnicodeDecodeError, IsADirectoryError):
         if callback is None:
-            click.echo(f'Unable to deserialise {path}', err=True)
+            echo(ctx, f'Unable to deserialise {path}', err=True)
             raise click.Abort
         else:
             try:
@@ -53,36 +67,46 @@ def deserialise(path, factory, callback=None, saveas=None):
                 mp = callback(path)
                 MapFactory.save_text(saveas, mp.serialise())
             except Exception as e:
-                click.echo(f'Unable to create map from {path} ({e.__name__}).', err=True)
+                echo(ctx, f'Unable to create map from {path} ({e.__name__}).', err=True)
                 raise click.Abort
 
 
 @click.group(invoke_without_command=False, context_settings=click_context)
-def cli():
+@click.pass_context
+@click.option('--pipe', is_flag=True, default=False,
+              help='Just output filenames - useful for scripting. WARNING: this will '
+                   'auto-accept everything and set the log level to FATAL.')
+def cli(ctx, pipe):
     """
     A command line interface for working with the linnaeus program.
 
     Run 'linnaeus <cmd> -h' for help with each subcommand.
     """
-    pass
+    ctx.obj = {
+        'pipe': pipe
+        }
+    if pipe:
+        silence()
 
 
 @cli.command(short_help='Creates a reference map or component map.')
+@click.pass_context
 @click.argument('inputs', type=click.Path(exists=True), nargs=-1)
 @click.option('-o', '--output', type=click.Path(),
               help='Map save path. Auto-generated if not supplied.')
-def makemap(inputs, output):
+def makemap(ctx, inputs, output):
     """
     Creates a reference map or component map.
 
     If a single input file path is given, a reference map will be made. If a directory
     path and/or multiple file paths are given, a component map will be made.
     """
+    from linnaeus import MapFactory
     iden = os.path.splitext(inputs[0].split(os.path.sep)[-1])[0]
 
     if len(inputs) > 1 or os.path.isdir(inputs[0]):
         output = output or MapFactory.component().defaultpath(iden)
-        setup_path(output)
+        setup_path(ctx, output)
 
         kwargs = {
             'folders': [],
@@ -94,7 +118,7 @@ def makemap(inputs, output):
             elif os.path.isdir(i):
                 kwargs['folders'].append(i)
             else:
-                click.echo(f'Ignoring {i}')
+                echo(ctx, f'Ignoring {i}')
         component_map = MapFactory.component().from_local(**kwargs)
         MapFactory.save_text(output, component_map.serialise())
     elif os.path.isfile(inputs[0]):
@@ -102,36 +126,40 @@ def makemap(inputs, output):
         try:
             Image.open(inputs)
         except OSError:
-            click.echo(f'Unable to open {inputs}.', err=True)
+            echo(ctx, f'Unable to open {inputs}.', err=True)
             raise click.Abort
         output = output or MapFactory.reference().defaultpath(iden)
-        setup_path(output)
+        setup_path(ctx, output)
         reference_map = MapFactory.reference().from_image_local(inputs)
         MapFactory.save_text(output, reference_map.serialise())
     else:
-        click.echo('Cannot identify target type.', err=True)
+        echo(ctx, 'Cannot identify target type.', err=True)
         raise click.Abort
-    click.echo(f'Saved map to {output}!')
+    click.echo(output)
 
 
 @cli.command(short_help='Displays some details about a serialised map file.')
+@click.pass_context
 @click.argument('path', type=click.Path(exists=True))
-def readmap(path):
+def readmap(ctx, path):
     """
     Displays some details about the given serialised map file. Can also act as a sort
     of validator for serialised map files in that if it can load them, they're valid.
     """
-    read_map = deserialise(path, MapFactory)
-    click.echo(click.style(path, bold=True))
-    click.echo(f'{type(read_map).__name__} with {len(read_map)} items')
+    from linnaeus import MapFactory
+    from linnaeus.models import ReferenceMap
+    read_map = deserialise(ctx, path, MapFactory)
+    echo(ctx, click.style(path, bold=True))
+    echo(ctx, f'{type(read_map).__name__} with {len(read_map)} items')
     if isinstance(read_map, ReferenceMap):
         w, h = read_map.bounds
-        click.echo(f'{w} "pixels" wide, {h} "pixels" tall')
-    click.echo('An example record:')
-    click.echo(read_map.records[0])
+        echo(ctx, f'{w} "pixels" wide, {h} "pixels" tall')
+    echo(ctx, 'An example record:')
+    echo(ctx, read_map.records[0])
 
 
 @cli.command(short_help='Attempts to solve the given reference and component set.')
+@click.pass_context
 @click.argument('ref', type=click.Path(exists=True))
 @click.argument('comps', type=click.Path(exists=True), nargs=-1)
 @click.option('-o', '--output', type=click.Path(),
@@ -143,7 +171,7 @@ def readmap(path):
 @click.option('--silhouette', is_flag=True, help='Create a silhouette image, i.e. not '
                                                  'matched on colour. Only really works '
                                                  'with references with transparency.')
-def solve(ref, comps, output, tolerance, silhouette):
+def solve(ctx, ref, comps, output, tolerance, silhouette):
     """
     Attempts to create a solution map for the given reference and component set.
 
@@ -152,12 +180,13 @@ def solve(ref, comps, output, tolerance, silhouette):
     an image file (for the reference) and one or more paths to image files and/or
     directories (for the component map), similar to the makemap command.
     """
+    from linnaeus import Builder, MapFactory, SolveError
     iden = os.path.splitext(ref.split(os.path.sep)[-1])[0].split('_')[0]
     output = output or MapFactory.solution().defaultpath(iden)
-    setup_path(output)
+    setup_path(ctx, output)
 
     # load and/or make maps
-    ref_map = deserialise(ref, MapFactory.reference(),
+    ref_map = deserialise(ctx, ref, MapFactory.reference(),
                           MapFactory.reference().from_image_local,
                           saveas=MapFactory.reference().defaultpath(iden))
 
@@ -171,14 +200,14 @@ def solve(ref, comps, output, tolerance, silhouette):
         elif os.path.isdir(i):
             kwargs['folders'].append(i)
         else:
-            click.echo(f'Ignoring {i}')
+            echo(ctx, f'Ignoring {i}')
     saveas = MapFactory.component().defaultpath(comps[0].split(os.path.sep)[-1])
     if len(comps) > 1:
         comp_map = MapFactory.component().from_local(**kwargs)
         MapFactory.save_text(saveas, comp_map.serialise())
     else:
         comps = comps[0]
-        comp_map = deserialise(comps, MapFactory.component(),
+        comp_map = deserialise(ctx, comps, MapFactory.component(),
                                lambda x: MapFactory.component().from_local(**kwargs),
                                saveas=saveas)
 
@@ -187,7 +216,7 @@ def solve(ref, comps, output, tolerance, silhouette):
         try:
             solution_map = Builder.silhouette(ref_map, comp_map)
         except SolveError as e:
-            click.echo(f'Something went wrong: {e}', err=True)
+            echo(ctx, f'Something went wrong: {e}', err=True)
             raise click.Abort
     else:
         tol = float(tolerance)
@@ -200,23 +229,26 @@ def solve(ref, comps, output, tolerance, silhouette):
                                              mask_tolerance=tol, use_mask=use_mask)
                 break
             except SolveError as e:
-                click.echo(e, err=True)
+                echo(ctx, e, err=True)
                 if not use_mask or attempts >= 5:
-                    click.echo('Nothing more to be done. Aborting.', err=True)
+                    echo(ctx, 'Nothing more to be done. Aborting.', err=True)
                     raise click.Abort
-                elif use_mask and tol < -0.5 and click.confirm(
-                        f'Increase tolerance to {tol/2}?', default=True):
+                elif use_mask and tol < -0.5 and confirm(ctx,
+                                                         f'Increase tolerance to '
+                                                         f'{tol/2}?',
+                                                         default=True):
                     tol /= 2
                 else:
-                    click.confirm('Disable mask?', abort=True, default=True)
+                    confirm(ctx, 'Disable mask?', abort=True, default=True)
                     use_mask = False
 
     if solution_map is not None:
         MapFactory.save_text(output, solution_map.serialise())
-        click.echo(f'Saved to {output}')
+        click.echo(output)
 
 
 @cli.command(short_help='Generate an image from a solution map.')
+@click.pass_context
 @click.argument('solution', type=click.Path(exists=True))
 @click.option('-o', '--output', type=click.Path(),
               help='Built image output path. Auto-generated if not given.')
@@ -225,7 +257,7 @@ def solve(ref, comps, output, tolerance, silhouette):
                    'match the reference.')
 @click.option('--prefix', type=click.Path(exists=True),
               help='The root directory of the components, either relative or absolute.')
-def render(solution, output, adjust, prefix):
+def render(ctx, solution, output, adjust, prefix):
     """
     Generates a jpg image from the given solution map.
 
@@ -233,25 +265,29 @@ def render(solution, output, adjust, prefix):
     generated in a different directory the relative paths will no longer be valid. If
     this is the case, use the '--prefix' flag to specify the location of the components.
     """
+    from linnaeus import Builder, MapFactory
     output = output or os.path.splitext(solution)[0] + '.jpg'
-    setup_path(output)
-    solution_map = deserialise(solution, MapFactory.solution())
+    setup_path(ctx, output)
+    solution_map = deserialise(ctx, solution, MapFactory.solution())
     canvas = Builder.fill(solution_map, adjust=adjust, prefix=prefix)
     canvas.save(output)
-    click.echo(f'Saved to {output}')
+    click.echo(output)
 
 
 @cli.command(short_help='Generate a .config file.')
-def makeconfig():
+@click.pass_context
+def makeconfig(ctx):
     constants.dump('.config')
 
 
 @cli.group()
-def prepro():
+@click.pass_context
+def prepro(ctx):
     pass
 
 
 @prepro.command(short_help='Remove the background from a reference image.')
+@click.pass_context
 @click.argument('image', type=click.Path(exists=True))
 @click.option('-c', '--colour', nargs=3, type=click.INT,
               help='Use this colour (RGB) as the background to remove. Auto-calculated '
@@ -274,12 +310,13 @@ def prepro():
 @click.option('-e', '--erode', type=click.INT, default=2,
               help='Erodes the mask before applying to remove colour edges and other '
                    'artefacts.')
-def removebg(image, colour, background, output, corners, fill_edges, holes, erode):
+def removebg(ctx, image, colour, background, output, corners, fill_edges, holes, erode):
     """
     Remove the background from an image (i.e. an image to be used as a reference) and
     save it. Intended for use on images with a clearly defined subject against a plain
     background.
     """
+    from linnaeus.preprocessing import BackgroundRemover, colourspace
     img = np.array(Image.open(image))
     if background is not None:
         bg = BackgroundRemover(img, bg_image=np.array(Image.open(background)))
@@ -295,4 +332,4 @@ def removebg(image, colour, background, output, corners, fill_edges, holes, erod
         bg.apply(bg.create_mask(fill_edges, holes, erosion=erode)))
     output = output or os.path.splitext(image)[0] + '_bg.png'
     masked_img.save(output)
-    click.echo(f'Saved to {output}')
+    click.echo(output)
